2db59a15107b21d7db9e4137b0e1a91e4076ab6a
minh <108040462+FPT-MinhND38@users.noreply.github.com>
2023-10-22 22:50:14 +0700

    sample flutter shoping

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,160 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyCustomButttonRealTime());
+  runApp(MaterialApp(
+    home: MaterialApp(
+      home: ShoppingList(
+        products: [
+          Product(name: "Iphone 12 promax"),
+          Product(name: "Iphone 11 promax"),
+          Product(name: "Iphone 7plus "),
+        ],
+      ),
+    )
+  ));
 }
 
-class MyCustomButttonRealTime extends StatelessWidget {
+class Product {
+  Product({required this.name});
+
+  final String name;
+}
+
+typedef CartChangeCallBack = Function(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangeCallBack onCartChanged;
+
+  ShoppingListItem({required this.product,
+    required this.inCart,
+    required this.onCartChanged})
+      : super(key: ObjectKey(product));
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme
+        .of(context)
+        .primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+    return TextStyle(
+        color: Colors.black54, decoration: TextDecoration.overline);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return LaunchScreent();
+    return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
+      leading: CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: Text(product.name[2]),
+      ),
+      title: Text(
+        product.name,
+        style: _getTextStyle(context),
+      ),
+    );
   }
 }
 
-class LaunchScreent extends StatefulWidget {
+class ShoppingList extends StatefulWidget {
+  ShoppingList({required this.products});
+
+  final List<Product> products;
+
   @override
   State<StatefulWidget> createState() {
-    return MyScreen();
+    return _ShoppingListState();
   }
 }
 
-class MyScreen extends State<LaunchScreent> {
-  bool stateTextClick = true;
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
 
-  Widget FirstWidget() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [Text("First String")],
-    );
-  }
-
-  Widget SecondWidget() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "Second String",
-        ),
-        Padding(
-          padding: EdgeInsets.only(right: 15),
-        ),
-      ],
-    );
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (stateTextClick) FirstWidget() else SecondWidget(),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      stateTextClick = !stateTextClick;
-                    });
-                  },
-                  child: Text("Click me"))
-            ],
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping Cart"),
       ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        children:
+        widget.products.map((product) {
+          return ShoppingListItem(
+              product: product,
+              inCart: _shoppingCart.contains(product),
+              onCartChanged: _handleCartChanged);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({required this.count, super.key});
+
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Count: $count");
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({required this.onPress, super.key});
+
+  late VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPress, child: Text("Increment"));
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void inCreateMent() {
+    _counter++;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CounterIncrementor(onPress: inCreateMent),
+        SizedBox(
+          width: 21,
+        ),
+        CounterDisplay(
+          count: _counter,
+        )
+      ],
     );
   }
 }
